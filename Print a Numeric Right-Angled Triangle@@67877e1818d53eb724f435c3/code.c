a9a897298aadc7f7f812ebbc817ec4040ab2957a
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-01-27 19:51:58 +0530

    Solution to Print a Numeric Right-Angled Triangle

--- Print a Numeric Right-Angled Triangle@@67877e1818d53eb724f435c3/code.c
+++ Print a Numeric Right-Angled Triangle@@67877e1818d53eb724f435c3/code.c
@@ -5,8 +5,7 @@ void main(){
 
     scanf("%d",&n); 
     for(int i=1;i<=n;i++){ 
-        for (int j=0;j<(n-i);j++){ 
-            printf(" "); }  
+    
         int a=1;
         for (int k=0;k<i;k++){ 
           
