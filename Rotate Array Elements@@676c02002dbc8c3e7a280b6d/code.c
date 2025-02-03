2c53e9300ee973043976275ee939c603904e1e6a
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-03 10:26:44 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -7,5 +7,5 @@ void main(){
     scanf("%d", &shift); 
     int temp[size]; 
     shift =shift%size;
-    for (int i=0;i<shift;i++){temp[i]=arr[(i+shift)%size];} 
+    for (int i=0;i<size;i++){temp[i]=arr[(i+shift)%size];} 
     for (int i=0;i<size;i++){printf("%d\n",temp[i]);} }
