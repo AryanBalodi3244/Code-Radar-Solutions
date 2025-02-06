c48e07c699260ad110ef80115b05800e82fc44f4
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-06 19:01:02 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -4,21 +4,13 @@ void main(){
     int size;
     scanf("%d",&size);
     int arr[size];
-    for (int i=0;i<size;i++){scanf("%d",&arr[i]);}
-        for (int i=0;i<size;i++){ 
-            for (int j=0;j<size-1;j++){ 
-                if (arr[j]>arr[j+1]){ 
-                    arr[j]=arr[j]^arr[j+1]; 
-                    arr[j+1]=arr[j]^arr[j+1]; 
-                    arr[j]=arr[j]^arr[j+1]; } }} 
-
-        int i=0;
-        while (arr[i]!='\0'){
-            if (arr[i+1]!=arr[i]+1 || arr[i+1]!=arr[i]-1 ){
-                printf("%d",arr[i]+1);
-                break;
-            }
-            i++;
-        }
-
-    } 
+for (int i=0;i<size;i++){scanf("%d",&arr[i]);}
+    for (int i=0;i<size;i++){ 
+        for (int j=0;j<size-1;j++){ 
+            if (arr[j]>arr[j+1]){ 
+                arr[j]=arr[j]^arr[j+1]; 
+                arr[j+1]=arr[j]^arr[j+1]; 
+                arr[j]=arr[j]^arr[j+1]; } }} 
+    for (int i=0;i<size;i++){ printf("%d ",arr[i]); 
+    
+    } }
