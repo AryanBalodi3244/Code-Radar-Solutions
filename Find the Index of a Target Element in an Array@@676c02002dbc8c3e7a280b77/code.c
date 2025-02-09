678b20ba7f6dca2afcd69c1caf3fbd5791ac7c24
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-09 19:44:34 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
-int binarySearch(int arr[], int l , int r , int target){
-    while (l<=r){
-        int mid = (l+r)/2;
-        if(arr[mid]==target){return mid;}
-        if  (arr[mid] !=target){
-            if (arr[mid]>target){
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
+int binarySearchFirst(int arr[], int l, int r, int target) {
+    int result = -1;  // Initialize result to -1 (not found)
     
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        
+        if (arr[mid] == target) {
+            result = mid;  // Found the target, store the index
+            r = mid - 1;  // Move left to find the first occurrence
+        } else if (arr[mid] > target) {
+            r = mid - 1;  // Search in the left half
+        } else {
+            l = mid + 1;  // Search in the right half
+        }
     }
-    return -1;
-
+    
+    return result;  // Return the first occurrence index or -1 if not found
 }
-void main(){
-    int size,target;
+
+int main() {
+    int size, target;
     scanf("%d", &size);
     int arr[size];
     for (int i = 0; i < size; i++) {
-            scanf("%d", &arr[i]);
+        scanf("%d", &arr[i]);
     }
-    scanf("%d",&target);
-    int k =binarySearch(arr,0 , size-1, target);
-    printf("%d",k);
+    scanf("%d", &target);
+
+    int k = binarySearchFirst(arr, 0, size - 1, target);
+    printf("%d\n", k);  // Print the index of the first occurrence or -1
 }
