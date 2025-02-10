fa3bc75c347fffc9e277b993d3717262cec6c27d
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-10 06:13:25 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
+
+struct Student {
+    int rno;
+    char name[10];
+    float marks;
+};
+
+
 void main(){
     int len;
     scanf("%d",&len);
-    int rno[len];
-    char name[len][10];
-    float marks[len];
+
     for (int i=0;i<len;i++){
-           scanf("%d %s %f",&rno[i],&name[i],&marks[i]);
-      
+           scanf("%d %s %f",&Student[i].rno,&Student[i].name,&Student[i].marks);
     }
     for (int i=0;i<len;i++){
            for (int j=i+1;i<len;i++){
-            if (marks[j]>marks[j+1]){
-                    marks[j]=marks[j]^marks[j+1];
-                     marks[j+1]=marks[j]^marks[j+1];
-                      marks[j]=marks[j]^marks[j+1];
-
-                       rno[j]=rno[j]^rno[j+1];
-                     rno[j+1]=rno[j]^rno[j+1];
-                      rno[j]=rno[j]^rno[j+1];
-
-                       name[j]=name[j]^name[j+1];
-                     name[j+1]=name[j]^name[j+1];
-                      name[j]=name[j]^name[j+1];
+            if (Student[j].marks>Student[j+1].marks){
+                
+                 struct Student temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
                 }
-
-        
     }
 
     }
