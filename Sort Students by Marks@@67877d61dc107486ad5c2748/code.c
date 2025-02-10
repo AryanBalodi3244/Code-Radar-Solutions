7cecb7ecf57edfaab6b3c54985c59d8c6e2a61fd
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-10 06:18:08 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -16,15 +16,13 @@ void main(){
     }
     for (int i=0;i<len;i++){
             for (int j = 0; j < len - i - 1; j++){
-            if (student[j].marks>student[j+1].marks){
+            if (student[j].marks<student[j+1].marks){
                 
                  struct Student temp = student[j];
                 student[j] = student[j + 1];
                 student[j + 1] = temp;
                 }
-    }
-
-    }
+    } }
         for (int i=0;i<len;i++){
                 printf("Roll Number: %d, Name: %s, Marks: %.2f\n",student[i].rno,student[i].name,student[i].marks);
         }
