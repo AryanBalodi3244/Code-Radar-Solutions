41f9b0411325bb77896fafe1061563d44908e41e
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-02-10 06:17:51 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -26,7 +26,7 @@ void main(){
 
     }
         for (int i=0;i<len;i++){
-                printf("Roll Number: %d, Name: %s, Marks: %.2f",student[i].rno,student[i].name,student[i].marks);
+                printf("Roll Number: %d, Name: %s, Marks: %.2f\n",student[i].rno,student[i].name,student[i].marks);
         }
 
 
