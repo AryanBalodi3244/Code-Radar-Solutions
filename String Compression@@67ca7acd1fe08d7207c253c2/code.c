9f826f2e335556a30623054a813e63f1382b955b
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-08 07:11:46 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -5,22 +5,17 @@ void compressString(char string[] ,char compressed[] ){
     int count =0;
     int i=0,j=0,start=0;
     while (string[i] !='\0'){
-        if (string[i+1] == string [i]){
-            if(start==0){
-                compressed[i]=string[i];
-                j=i;
-                start=1;           
-                
-                 }
-            count++;
-        }
+        compressed[j++]=string [i];
+        count =1;
+
+       while  (string[i+1] == string[i]){
+        count++;
+        i++;
+       }
         else if (count>1) {
-            sprintf(compressed + strlen(compressed), "%d", count);
-            count =0;
+           j+= sprintf(compressed + j, "%d", count);
         }
-        else if (count ==0){
-            compressed[i]=string[i];
-        }
+       
         i++;
 
 
