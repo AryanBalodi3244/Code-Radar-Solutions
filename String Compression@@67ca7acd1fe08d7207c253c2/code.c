9a753f9b1039501a46099c01c8bbb85ce46e0c1f
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-08 07:07:16 +0530

    Solution to String Compression

---
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -0,0 +1,27 @@
+void compressString(char string[] ,char compressed[] ){
+    
+    int count =0;
+    int i=0,j=0,start=0;
+    while (string[i] !='\0'){
+        if (string[i+1] == string [i]){
+            if(start==0){
+                compressed[i]=string[i];
+                j=i;
+                start=1;           
+                
+                 }
+            count++;
+        }
+        else if (count>1) {
+            sprintf(compressed + strlen(compressed), "%d", count);
+            count =0;
+        }
+        else if (count ==0){
+            compressed[i]==string[i]
+        }
+        i++;
+
+
+    }
+
+}
