7d6eca14954f2ce82ea3a121e08b233abb540f83
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-09 08:48:15 +0530

    Solution to Insertion Sort for Characters

---
+++ Insertion Sort for Characters@@67c9a8aa6e98bd154cc82f42/code.c
@@ -0,0 +1,19 @@
+void selectionSort(char arr[], int n){
+    for (int step=0;step<n-1;step++){
+        int min_idx =step;
+        for (int i =step+1 ; i<n ; i++){
+            if (arr[i]<arr[min_idx]){
+                min_idx = i;
+            }
+        }
+        if (min_idx != step) {
+        arr[min_idx] ^= arr[step] ^= arr[min_idx] ^= arr[step];
+    }    
+}
+}
+
+void printArray(char arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+}
