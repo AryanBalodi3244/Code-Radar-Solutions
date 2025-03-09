945163d2d81e535e6c39c2a0676bf1b2b8eb776d
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-09 08:52:06 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,25 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int bubbleSort(char *arr[] ,int size){
+int bubbleSort(char *arr[], int size) {
     int swapped;
-       for (int i=0;i<size;i++){
+    if (arr == NULL || size <= 0) {
+        return -1; // Error checking
+    }
+    
+    for (int i = 0; i < size; i++) {
         swapped = 0;
-         for (int j=0;j<size-i-1;j++){
-       
-            if (arr[j][0] >arr[j + 1][0]) {
-                
+        for (int j = 0; j < size - i - 1; j++) {
+            // Make sure both strings exist before comparing
+            if (arr[j] == NULL || arr[j + 1] == NULL) {
+                continue; // Skip NULL entries
+            }
+            
+            if (arr[j][0] > arr[j + 1][0]) {
                 char *temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 swapped = 1;  
             }
         }
-        if (!swapped) break; 
-       } 
-       return 0;
-}
-void printArray(char *arr[],int size){
-        for (int i=0;i<size;i++){
-        printf("%s\n",arr[i]);
+        if (!swapped) break;
     }
+    return 0;
+}
 
+void printArray(char *arr[], int size) {
+    if (arr == NULL || size <= 0) {
+        return; // Error checking
+    }
+    
+    for (int i = 0; i < size; i++) {
+        if (arr[i] != NULL) {
+            printf("%s\n", arr[i]);
+        }
+    }
 }
