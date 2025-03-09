af1c8afbedfb3dcbd299c7b617aa4a72dbd16391
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-09 09:35:42 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,34 +1,23 @@
-void bubbleSort(char *arr[] ,int size){
+void bubbleSort(char arr[][100] ,int size){
     int swapped;
-       for (int i=0;i<size-1;i++){
+       for (int i=0;i<size;i++){
         swapped = 0;
-   
-          for (int j = 0; j < size - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {  
-                char *temp = arr[j];
+         for (int j=0;j<size-i-1;j++){
+       
+            if (arr[j][0] >arr[j + 1][0]) {
+                
+                char temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                swapped = 1;
+                swapped = 1;  
             }
         }
         if (!swapped) break; 
        } 
 }
-
-// void printArray(char *arr[], int size) {
-//     for (int i = 0; i < size; i++) {
-       
-//             printf("%s\n", arr[i]);  
-//         }
-    
-// }
-
-void printArray(char arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == NULL) {  // Check for NULL values
-            printf("[NULL STRING]\n");
-        } else {
-            printf("%s\n", arr[i]);  // Print each string
-        }
+void printArray(char arr[][100],int size){
+        for (int i=0;i<size;i++){
+        printf("%s\n",arr[i]);
     }
+
 }
