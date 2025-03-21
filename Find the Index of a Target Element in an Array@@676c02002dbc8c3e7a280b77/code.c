ed9334f974d925970d10233e700f4314a839db1b
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-21 14:40:54 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -4,13 +4,15 @@ int binarySearchFirst(int arr[], int l, int r, int target) {
     int result =-1;
     while (l<=r){
         int mid = (l+r)/2;
+        if (mid == target) {
+            result = mid;
+        }
         if (mid > target){
             l= mid+1;
         }
         else if (mid < target) r= mid-1;
-        else{
-            result = mid;
-        }
+
+       
     }
         return result;
 }
