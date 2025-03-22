8911582f2389eccc037f5e58fa82ab10647aa0db
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-22 06:29:33 +0530

    Solution to Count Distinct Substrings

--- Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int is_unique(char substr[][100], char k[] , int len) {
+  for(int i=0; i<len ; i++){
+    if (strcmp(substr[i] , k)==0){
+        return 0;
+    }
+  }
+  return 1;
+}
 
 int main() {
     char str[100], substr[5000][100];  
@@ -15,7 +22,7 @@ int main() {
         char k[20];
         strncpy(k , str+i , j-i+1);
         k[j - i + 1] = '\0';
-        if (strcmp(substr[i] , k)==0){
+        if (is_unique(substr , k , n)){
             strcpy(substr[n++], k);
         }
     }
