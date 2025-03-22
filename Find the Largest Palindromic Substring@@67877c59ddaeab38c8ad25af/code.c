2865e69fb983dd6354327dda7ce3a5b0a983d870
AryanBalodi3244 <aryan0061.becse24@chitkara.edu.in>
2025-03-22 06:07:04 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -13,6 +13,7 @@ int main() {
     return 0;
     int len =strlen(s);
     int maxLen= -1;
+    int paliLen;
     for(int i=0 ; i<len;i++){
         int end = len-1;
         for (int j =end ; i>=i ; i++ ){
